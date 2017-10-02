5cb48051f540d7a736fa08c427abc008d0422ea5
Dmitry Polisadov <dmitry@polisadov.ru>
2017-10-03 01:23:06 +0300

    Add ability to edit/update topics

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
@@ -11,14 +13,38 @@ defmodule Discuss.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} -> render conn, "new.html", changeset: changeset
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic created")
+        |> redirect(to: topic_path(conn, :index))
       {:error, changesets} -> render conn, "new.html", changeset: changesets
     end
-    render conn, "new.html", changeset: changeset
+
   end
 
   def index(conn, params) do
     topics = Repo.all(Topic)
     render conn, "index.html", topics: topics
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "topic updated")
+        |> redirect(to: topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: old_topic
+    end
+    redirect(conn, to: topic_path(conn, :index))
   end
 end
