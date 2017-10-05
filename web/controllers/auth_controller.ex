9ca1fefb7fdda22317ea608101892812f50afe93
Dmitry Polisadov <dmitry@polisadov.ru>
2017-10-06 00:11:52 +0300

    finish with github oAuth

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -4,14 +4,30 @@ defmodule Discuss.AuthController do
 
   alias Discuss.User
 
-  def callback(%{assigns: %{ueberauth_auth: auth}}, params) do
-    user_params = %{token: auth.credential.token, email: auth.info.email, provider: "github"}
-    changeset = changeset(%User{},user_params)
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case inser_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 
   defp inser_or_update_user(changeset) do
     case Repo.get_by(User, email: changeset.changes.email) do
-      nil -> Repo.Insert(changeset)
+      nil -> Repo.insert(changeset)
       user -> {:ok, user}
     end
   end
