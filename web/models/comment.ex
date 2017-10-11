c4c41b3c391efebbf4fdc4a0c8dd9ab43e1b015b
Dmitry Polisadov <dmitry@polisadov.ru>
2017-10-11 16:08:26 +0300

    add comments section

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -1,16 +1,19 @@
 defmodule Discuss.Comment do
   use Discuss.Web, :model
 
+  @derive {Poison.Encoder, only: [:content]}
+
   schema "comments" do
     field :content, :string
     belongs_to :user, Discuss.User
     belongs_to :topic, Discuss.Topic
+    timestamps()
   end
 
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:title])
-    |> validate_required([:title])
-    |> validate_length(:title, min: 3)
+    |> cast(params, [:content])
+    |> validate_required([:content])
+    |> validate_length(:content, min: 3)
   end
 end
--- web/static/js/socket-compiled.js
+++ web/static/js/socket-compiled.js
@@ -3,9 +3,9 @@
 
 // To use Phoenix channels, the first step is to import Socket
 // and connect at the socket path in "lib/my_app/endpoint.ex":
-import {Socket} from "phoenix"
+import { Socket } from "phoenix";
 
-let socket = new Socket("/socket", {params: {token: window.userToken}})
+let socket = new Socket("/socket", { params: { token: window.userToken } });
 
 // When you connect, you'll often need to authenticate the client.
 // For example, imagine you have an authentication plug, `MyAuth`,
@@ -52,28 +52,26 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 // Or, remove it from the constructor if you don't care about
 // authentication.
 
-socket.connect()
+socket.connect();
 
-const createSocket = (topicId) => {
-    let channel = socket.channel(`comments:${topicId}`, {})
-    channel.join()
-        .receive("ok", resp => {
-            renderComments(resp.comments);
-        })
-        .receive("error", resp => {
-            console.log("Unable to join", resp)
-        })
+const createSocket = topicId => {
+    let channel = socket.channel(`comments:${topicId}`, {});
+    channel.join().receive("ok", resp => {
+        renderComments(resp.comments);
+    }).receive("error", resp => {
+        console.log("Unable to join", resp);
+    });
 
     document.querySelector('button').addEventListener('click', () => {
-        const content = document.querySelector('textarea').value
+        const content = document.querySelector('textarea').value;
 
-        channel.push('comment:add', {content: content})
+        channel.push('comment:add', { content: content });
     });
-}
+};
 
 function renderComments(comments) {
     const renderedComments = comments.map(comment => {
-       return `
+        return `
             <li class="collection-item">
                 ${comment.content}
             </li>
@@ -81,7 +79,8 @@ function renderComments(comments) {
     });
 
     document.querySelector('.collection').innerHTML = renderedComments.join('');
-
 }
 
 window.createSocket = createSocket;
+
+//# sourceMappingURL=socket-compiled.js.map
